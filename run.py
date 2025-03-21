eb99c1f35e6429857e7a4816b13ea559d379ce60
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-03-21 10:22:46 +0000

    feat: add basic candidate data file

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 
@@ -7,7 +8,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    data = []
+    with open("data/candidates.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("index.html", candidates=data)
 
 
 @app.route("/about")
