0b019669aa8fd4b037b84e842ac040b969255f49
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-03-21 13:03:56 +0000

    feat: URL with template for candidates in database

--- run.py
+++ run.py
@@ -24,6 +24,22 @@ def jobad():
     return render_template("jobad-generator.html")
 
 
+@app.route("/candidates")
+def candidates():
+    return render_template("candidates.html")
+
+
+@app.route("/candidates/<candidate_name>")
+def about_candidate(candidate_name):
+    candidate = {}
+    with open("data/candidates.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == candidate_name:
+                candidate = obj
+    return render_template("candidates.html", candidate=candidate)
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
