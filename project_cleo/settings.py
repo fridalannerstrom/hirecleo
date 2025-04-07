30ce951ee925f67ba82257043c9c09f3f7eaa3bd
Frida Lannerström <frida@tqnordic.com>
2025-04-07 17:37:48 +0200

    feat: prepare for deployment on heroku

--- project_cleo/settings.py
+++ project_cleo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c27de-twl@f8-n6!eck$1khe329gloz6-9yp(9r&)%gh++jn=3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
