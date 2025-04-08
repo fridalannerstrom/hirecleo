e4495483ca61e41309a1b57a2016cdabb20e2efd
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-04-08 12:55:55 +0200

    feat: add models for candidates and jobs

--- hello_world/admin.py
+++ hello_world/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Candidate, Job
 
 # Register your models here.
+admin.site.register(Candidate)
+admin.site.register(Job)
--- hello_world/models.py
+++ hello_world/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Job(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="jobs")
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Candidate(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="candidates")
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    linkedin_url = models.URLField(blank=True)
+    cv_text = models.TextField(blank=True)
+    interview_notes = models.TextField(blank=True)
+    test_results = models.TextField(blank=True)
+    jobs = models.ManyToManyField(Job, related_name="candidates")
+    slug = models.SlugField(max_length=200, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
