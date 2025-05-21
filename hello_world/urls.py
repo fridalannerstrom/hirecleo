dee15d21552b2ca90e0173eeaff4761091e0cbfb
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-05-21 13:03:14 +0200

    feat: basic html signup page

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
     path('candidates/<slug:slug>/', views.candidate_detail, name='candidate_detail'),
     path('candidates/<slug:slug>/edit/', views.edit_candidate, name='edit_candidate'),
     path('candidates/<slug:slug>/delete/', views.delete_candidate, name='delete_candidate'),
+    path('skapa-konto/', auth_views.LoginView.as_view(template_name='auth-register-basic.html'), name='register'),
 ]
