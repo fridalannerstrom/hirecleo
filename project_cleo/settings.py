b7db82d9dd938d3aea09fb10c5d71db91c6ae447
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-06-26 18:08:26 +0200

    trigger push

--- project_cleo/settings.py
+++ project_cleo/settings.py
@@ -99,11 +99,7 @@ WSGI_APPLICATION = 'project_cleo.wsgi.application'
 # }
 
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL", ""),
-        conn_max_age=600,
-        ssl_require=True  # säkrare anslutning
-    )
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 CSRF_TRUSTED_ORIGINS = [
